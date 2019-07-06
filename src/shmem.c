cc88549dcc91ac2d0b31a08cf90fd342f665b151
Chikashi Miyama <chikashimiyama@gmail.com>
2019-07-06 13:00:35 +0200

    integrate shmem function in patch

--- src/shmem.c
+++ src/shmem.c
@@ -1,3 +1,4 @@
+#include <windows.h>
 #include "m_pd.h"
 
 static t_class *shmem_class;
@@ -5,47 +6,88 @@ static t_class *shmem_class;
 typedef struct _shmem {
 	t_object  x_obj;
 	t_symbol *x_arrayname;
+
+	DWORD size_;
+	HANDLE hMapFile_;
+	LPVOID pBuf_;
 } t_shmem;
+
+t_garray* findArray(t_symbol* name)
+{
+	return (t_garray*)pd_findbyclass(name, garray_class);
+}
 
 void shmem_bang(t_shmem *x)
 {
-	t_garray *a;
 	int npoints;
-	t_word *vec;
-
-	if (!(a = (t_garray *)pd_findbyclass(x->x_arrayname, garray_class)))
+	t_float *vec;
+	t_garray *array = findArray(x->x_arrayname);
+	if (!array)
 	{
 		pd_error(x, "%s: no such array", x->x_arrayname->s_name);
 		return;
 	}
 
-	if (!garray_getfloatwords(a, &npoints, &vec))
+	if (!garray_getfloatarray(array, &npoints, &vec))
 	{
 		pd_error(x, "%s: bad template for shmem", x->x_arrayname->s_name);
 		return;
 	}
 
+	int pointsToProcess = garray_npoints(array);
+	if (pointsToProcess < (int)x->size_)
+		pointsToProcess = (int)x->size_;
 
-	post("%f", vec[0].w_float);
-
+	memcpy(x->pBuf_, array, pointsToProcess * sizeof(float));
 }
 
-void *shmem_new(t_symbol *s)
+void *shmem_new(t_symbol *s, t_float numSamples)
 {
+	if (numSamples < 1.0f)
+	{
+		error("at lease one sample of data is required");
+		return NULL;
+	}
+
 	t_shmem *x = (t_shmem *)pd_new(shmem_class);
 	x->x_arrayname = s;
-	outlet_new(&x->x_obj, &s_float);
 
+	x->hMapFile_ = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, x->size_ * sizeof(float), s->s_name);
+	if (x->hMapFile_ == NULL)
+	{
+		pd_error(x, "cannot create managed file");
+		return NULL;
+	}
+
+	x->pBuf_ = MapViewOfFile(x->hMapFile_, FILE_MAP_ALL_ACCESS, 0, 0, x->size_ * sizeof(float));
+
+	if (x->pBuf_ == NULL)
+	{
+		CloseHandle(x->hMapFile_);
+		pd_error(x, "cannot map view of file");
+	}
+
+	x->size_ = numSamples;
 	return (void *)x;
+}
+
+void shmem_delete(t_shmem* x)
+{
+	UnmapViewOfFile(x->pBuf_);
+	CloseHandle(x->hMapFile_);
 }
 
 void shmem_setup(void) {
 
-	shmem_class = class_new(gensym("shmem"),
+	shmem_class = class_new(
+		gensym("shmem"),
 		(t_newmethod)shmem_new,
-		0, sizeof(t_shmem),
-		0, A_DEFSYM, 0);
+		(t_method)shmem_delete, 
+		sizeof(t_shmem),
+		0, A_SYMBOL, A_FLOAT, 0);
 
 	class_addbang(shmem_class, shmem_bang);
 }
+
+
 
